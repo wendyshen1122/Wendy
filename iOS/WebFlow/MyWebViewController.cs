aa3871151b161aa1592f47742e7f5ff1ec8ca087
WendyShen <wendy.shen@thinkpower-info.com>
2016-11-22 14:52:49 +0800

    我的第一支iOS

---
+++ iOS/WebFlow/MyWebViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace Wendy.iOS
+{
+	public partial class MyWebViewController : UIViewController
+	{
+		public MyWebViewController() : base("MyWebViewController", null)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
